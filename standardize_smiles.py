04f2cd268ffed408731e5ebad650bab57c78119f
Alan Christie <alan.christie@matildapeak.com>
2021-03-18 15:07:23 +0000

    - Now just Python 3.8
    - Adds pylint
    - Fixed pylint errors/warnings

--- standardize_smiles.py
+++ standardize_smiles.py
@@ -3,7 +3,8 @@
 
 """standardize_smiles.py
 
-Utility to standardize an input smiles into the standard representation used in the fragment network.
+Utility to standardize an input smiles into the standard representation
+used in the fragment network.
 
 The module provides methods for rendering a vendor (original) SMILES
 representation into a standard smiles format in isomeric or nonisomeric form
@@ -11,18 +12,20 @@ The nonisomeric form is used for searching within the fragment network.
 
 Dependencies
 ------------
-Note that this is heavily dependent on RDKIT.
+Note that this is heavily dependent on RDKit.
 
 Duncan Peacock
 March 2021
 """
 
+import logging
+
 # RDKit
+# pylint: disable=E0401
 from rdkit import Chem
 from rdkit.Chem.MolStandardize import rdMolStandardize
 
-# Out logger
-import logging
+# Our logger
 logger = logging.getLogger(__name__)
 
 
@@ -33,18 +36,17 @@ def get_biggest_component(mol):
 
     if len(frags) == 1:
         return mol
-    else:
-        i = 0
 
-        biggest_count = 0
-        for frag in frags:
-            hac = frag.GetNumHeavyAtoms()
-            if hac > biggest_count:
-                biggest_count = hac
-                biggest_mol = frag
-            i += 1
+    i = 0
+    biggest_count = 0
+    for frag in frags:
+        hac = frag.GetNumHeavyAtoms()
+        if hac > biggest_count:
+            biggest_count = hac
+            biggest_mol = frag
+        i += 1
 
-        return biggest_mol
+    return biggest_mol
 
 
 def remove_isotopes(mol):
@@ -68,8 +70,8 @@ def standardize_format(mol):
 
 
 def standardize_molecule(mol, osmiles):
-    """Standardise a RDKit mol object. A SMILES string that represents the molecule can also be passed in to allow
-    better logging of errors.
+    """Standardise a RDKit mol object. A SMILES string that represents the
+    molecule can also be passed in to allow better logging of errors.
 
     :param mol: flag indicating whether this is a molecule or not.
     :param osmiles: The original (non-standard) SMILES
@@ -79,53 +81,51 @@ def standardize_molecule(mol, osmiles):
      """
 
     std = None
-
     try:
         std = standardize_format(mol)
-    except Exception as e:
+    except Exception as ex:
         logger.warning('standardize(%s) exception: "%s"',
-                       osmiles, e.message)
+                       osmiles, ex.message)
 
-    if std:
-        return std
-    else:
+    if not std:
         logger.error('Got nothing from standardize(%s).'
                      ' Skipping this compound', osmiles)
-        return
+    return std
 
 
-def get_smiles(isomericSmiles, std_molecule, osmiles):
+def get_smiles(isomeric_smiles, std_molecule, osmiles):
     """Return Smiles of desired format given a standardised molecule
     """
 
+    smiles = None
     try:
-        smiles = Chem.MolToSmiles(std_molecule, isomericSmiles, canonical=True)
-    except Exception as e:
+        smiles = Chem.MolToSmiles(std_molecule, isomeric_smiles, canonical=True)
+    except Exception as ex:
         logger.warning('MolToSmiles(%s, noniso) exception: "%s"',
-                       osmiles, e.message)
-    if smiles:
-        return smiles
-    else:
+                       osmiles, ex.message)
+    if not smiles:
         logger.error('Got nothing from MolToSmiles(%s, noniso).'
                      ' Skipping this compound', osmiles)
-        return
+    return smiles
 
 
-def process_osmiles(osmiles, isomericSmiles):
-    """Takes original smiles and processes to return a smiles of the desired format (isomeric or nonisomeric)
-    with the standardized molecule
+def process_osmiles(osmiles, isomeric_smiles):
+    """Takes original smiles and processes to return a smiles of the
+    desired format (isomeric or nonisomeric) with the standardized molecule
     """
     mol = None
     try:
         mol = Chem.MolFromSmiles(osmiles)
-    except Exception as e:
+    except Exception as ex:
         logger.warning('MolFromSmiles(%s) exception: "%s"',
-                       osmiles, e.message)
+                       osmiles, ex.message)
 
+    smiles = None
+    std_molecule = None
     if mol:
         std_molecule = standardize_molecule(mol, osmiles)
         if std_molecule:
-            smiles = get_smiles(isomericSmiles, std_molecule, osmiles)
+            smiles = get_smiles(isomeric_smiles, std_molecule, osmiles)
     else:
         logger.error('Got nothing from MolFromSmiles(%s).'
                      ' Skipping this compound', osmiles)
@@ -145,7 +145,7 @@ def standardize_to_iso_smiles(osmiles):
     On error, the standard form will be returned as None.
     """
     if not osmiles:
-        return
+        return None
 
     return process_osmiles(osmiles, True)
 
@@ -162,6 +162,6 @@ def standardize_to_noniso_smiles(osmiles):
     On error, the standard form will be returned as None.
     """
     if not osmiles:
-        return
+        return None
 
     return process_osmiles(osmiles, False)
